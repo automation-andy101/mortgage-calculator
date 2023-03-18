9daac4a5b2d71a3c11ade83082500118f65e3f9a
Andy Short <automation.andy101@gmail.com>
2023-03-18 21:23:23 +0700

    Added Navbar typography

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,20 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
+import { Container } from '@mui/material';
 
 
 const Navbar = () => {
   return (
     <div>
       <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Bank of React
-          </Typography>
-        </Toolbar>
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Typography variant="h5" component="p">
+              Bank of React
+            </Typography>
+          </Toolbar>
+        </Container>
       </AppBar>
     </div>
   )
